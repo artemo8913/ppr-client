b4d886a3aca4523d3c9c025222636b0a5ed000b9
LAPTOP-L9O08U7Q\kalin <kaline-son@ya.ru>
2022-12-22 17:46:41 +0700

    tests for get api

---
+++ src/Components/ButtonForApiTest.tsx
@@ -0,0 +1,14 @@
+import apiFetch from "../healper/ApiFetch";
+
+export default function ButtonForApiTest(props: {
+  url: string;
+  callback: Function;
+  method: string;
+}) {
+  const { url, callback, method } = props;
+  return (
+    <button onClick={() => apiFetch.getData(url, callback, method)}>
+      {method + " " + url}
+    </button>
+  );
+}
--- src/Components/DeveloperPage.tsx
+++ src/Components/DeveloperPage.tsx
@@ -1,46 +1,128 @@
 import React from "react";
-import apiFetch from "../healper/ApiFetch";
+import Button from "./ButtonForApiTest";
 
 const baseUrl = "http://localhost:5000/api";
 
 export default function DeveloperPage() {
-  const [workApiResult, setWorkApiResult] =
-    React.useState<string>("workApiResult");
-  const [pprApiResult, setPprApiResult] =
-    React.useState<string>("pprApiResult");
+  const [workApiResult, setWorkApiResult] = React.useState("work");
+  const [workApiSettings, setWorkApiSettings] = React.useState({
+    id: 1,
+  });
+  const [pprApiResult, setPprApiResult] = React.useState("ppr");
+  const [pprApiSettings, setPprApiSettings] = React.useState({
+    id: 1,
+  });
+  const [divisionsApiResult, setDivisionsApiResult] = React.useState("div");
+  const [divisionsApiSettings, setDivisionsApiSettings] = React.useState({
+    divisionTableName: "directions",
+    id: 1,
+  });
+  function changeWorkApiSettings(divisionsApi: { id?: number }) {
+    setWorkApiSettings((prev) => {
+      return {
+        ...prev,
+        ...divisionsApi,
+      };
+    });
+  }
+  function changePprApiSettings(divisionsApi: { id?: number }) {
+    setPprApiSettings((prev) => {
+      return {
+        ...prev,
+        ...divisionsApi,
+      };
+    });
+  }
+  function changeDivisionsApiSettings(settings: {
+    divisionTableName?: string;
+    id?: number;
+  }) {
+    setDivisionsApiSettings((prevSettings) => {
+      return {
+        ...prevSettings,
+        ...settings,
+      };
+    });
+  }
+
   return (
     <div>
       <h2>Страница для разработчика</h2>
       <fieldset>
         <legend>API запросы по работам</legend>
-        <button
-          onClick={() =>
-            apiFetch.getData(baseUrl + "/work/all", setWorkApiResult, "get")
-          }
-        >
-          get("/all", getAllWorkData)
-        </button>
+        <input
+          onChange={(e) => changeWorkApiSettings({ id: Number(e.target.value) })}
+          value={workApiSettings.id}
+          type="number"
+        />
+        <Button
+          url={baseUrl + "/work"}
+          callback={setWorkApiResult}
+          method="get"
+        ></Button>
+        <Button
+          url={baseUrl + `/work/${workApiSettings.id}`}
+          callback={setWorkApiResult}
+          method="get"
+        ></Button>
+        <br />
         {workApiResult}
       </fieldset>
       <fieldset>
         <legend>API запросы по ППРам</legend>
-        <button
-          onClick={() =>
-            apiFetch.getData(baseUrl + "/ppr/all", setPprApiResult, "get")
-          }
-        >
-          get("/all", getAllPlans)
-        </button><br />
-        <button
-          onClick={() =>
-            apiFetch.getData(baseUrl + "/ppr/", setPprApiResult, "get", {
-              nameOfPprTable: "pprechk9",
+        <input
+          onChange={(e) => changePprApiSettings({ id: Number(e.target.value) })}
+          value={pprApiSettings.id}
+          type="number"
+        />
+        <Button
+          url={baseUrl + "/ppr/"}
+          callback={setPprApiResult}
+          method="get"
+        ></Button>
+        <Button
+          url={baseUrl + `/ppr/${pprApiSettings.id}`}
+          callback={setPprApiResult}
+          method="get"
+        ></Button>
+        <br />
+        {pprApiResult}
+      </fieldset>
+      <fieldset>
+        <legend>API запросы по подразделениям</legend>
+        <select
+          onChange={(e) =>
+            changeDivisionsApiSettings({
+              divisionTableName: e.target.value,
             })
           }
         >
-          get("/", getYearPlan)
-        </button>
-        {pprApiResult}
+          <option>directions</option>
+          <option>distances</option>
+          <option>subdivisions</option>
+        </select>
+        <input
+          onChange={(e) =>
+            changeDivisionsApiSettings({ id: Number(e.target.value) })
+          }
+          value={divisionsApiSettings.id}
+          type="number"
+        />
+        <Button
+          url={baseUrl + "/divisions/"}
+          callback={setDivisionsApiResult}
+          method="get"
+        ></Button>
+        <Button
+          url={
+            baseUrl +
+            `/divisions/${divisionsApiSettings.divisionTableName}/${divisionsApiSettings.id}`
+          }
+          callback={setDivisionsApiResult}
+          method="get"
+        ></Button>
+        <br />
+        {divisionsApiResult}
       </fieldset>
     </div>
   );
