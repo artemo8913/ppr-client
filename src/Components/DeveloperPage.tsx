f54730ec9db8722c0ba127cab9632660dc86c118
LAPTOP-L9O08U7Q\kalin <kaline-son@ya.ru>
2022-12-22 13:01:24 +0700

    dev page

--- src/Components/DeveloperPage.tsx
+++ src/Components/DeveloperPage.tsx
@@ -1,13 +1,47 @@
 import React from "react";
 import apiFetch from "../healper/ApiFetch";
 
+const baseUrl = "http://localhost:5000/api";
+
 export default function DeveloperPage() {
-  React.useEffect(() => {
-    apiFetch.getData("http://localhost:5000/api/ppr/all", console.log);
-  });
+  const [workApiResult, setWorkApiResult] =
+    React.useState<string>("workApiResult");
+  const [pprApiResult, setPprApiResult] =
+    React.useState<string>("pprApiResult");
   return (
     <div>
       <h2>Страница для разработчика</h2>
+      <fieldset>
+        <legend>API запросы по работам</legend>
+        <button
+          onClick={() =>
+            apiFetch.getData(baseUrl + "/work/all", setWorkApiResult, "get")
+          }
+        >
+          get("/all", getAllWorkData)
+        </button>
+        {workApiResult}
+      </fieldset>
+      <fieldset>
+        <legend>API запросы по ППРам</legend>
+        <button
+          onClick={() =>
+            apiFetch.getData(baseUrl + "/ppr/all", setPprApiResult, "get")
+          }
+        >
+          get("/all", getAllPlans)
+        </button><br />
+        <button
+          onClick={() =>
+            apiFetch.getData(baseUrl + "/ppr/", setPprApiResult, "get", {
+              nameOfPprTable: "pprechk9",
+            })
+          }
+        >
+          get("/", getYearPlan)
+        </button>
+        {pprApiResult}
+      </fieldset>
     </div>
   );
 }
--- src/healper/ApiFetch.tsx
+++ src/healper/ApiFetch.tsx
@@ -1,8 +1,13 @@
 class ApiFetch {
-  async getData(url: string, callback: Function, data?: object | Array<any>) {
-    await fetch(url, { body: JSON.stringify(data) })
+  async getData(
+    url: string,
+    callback: Function,
+    method: string,
+    data?: object | Array<any>
+  ) {
+    await fetch(url, { body: JSON.stringify(data), method })
       .then((data) => data.json())
-      .then((json) => callback(json))
+      .then((json) => callback(JSON.stringify(json)))
       .catch((error) => console.log(error));
   }
 }
