af4663ff51b3021d9b0560e805a3ac26cd14c945
Spiryaev <kaline-son@ya.ru>
2023-05-13 12:11:46 +0700

    little changes

--- src/widgets/Top/ui/Top.tsx
+++ src/widgets/Top/ui/Top.tsx
@@ -1,6 +1,7 @@
 import css from "./Top.module.scss";
 import { SidebarSwitcher } from "widgets/SidebarSwitcher";
 import { createClassName } from "shared/lib/createClassName";
+import { useTranslation } from "react-i18next";
 
 interface TopProps {
   additionalClassName?: string;
@@ -9,10 +10,11 @@ interface TopProps {
 }
 
 function Top({ additionalClassName, isSidebarOpen, toggleSidebar }: TopProps) {
+  const { t } = useTranslation();
   return (
     <header className={createClassName(css.Top, {}, [additionalClassName])}>
       <SidebarSwitcher isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-      <h1>Цифровая дистанция электроснабжения. ППР</h1>
+      <h1>{t("main_title")}</h1>
     </header>
   );
 }
