0142df31ae065105e9adf289b39b3f4ee7eb527c
Spiryaev <kaline-son@ya.ru>
2023-05-14 20:32:04 +0700

    fix sidebar

--- src/processes/Sidebar/ui/Sidebar.test.tsx
+++ src/processes/Sidebar/ui/Sidebar.test.tsx
@@ -1,10 +1,11 @@
-import { screen, render } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import { Sidebar } from "./Sidebar";
 import { renderWith } from "shared/test/renderWith";
 
 describe("SidebarTest", () => {
   test("SidebarIsShow", () => {
     renderWith(<Sidebar isOpen={true} />);
+    screen.debug();
     expect(screen.getByTestId("sidebar")).toBeInTheDocument();
   });
 });
--- src/processes/Sidebar/ui/Sidebar.tsx
+++ src/processes/Sidebar/ui/Sidebar.tsx
@@ -9,6 +9,7 @@ import { createClassName } from "shared/lib/createClassName";
 import HomeIcon from "shared/assets/icons/homeIcon.svg";
 import ContactSupportIcon from "shared/assets/icons/contactSupport.svg";
 import { LanguageSwitcher } from "widgets/LanguageSwitcher";
+import { useTranslation } from "react-i18next";
 
 interface SidebarProps {
   additionalClassName?: string;
@@ -16,15 +17,16 @@ interface SidebarProps {
 }
 
 function Sidebar({ additionalClassName, isOpen }: SidebarProps) {
+  const { t } = useTranslation();
   return (
     <div data-testid="sidebar" className={createClassName(css.Sidebar, { [css.open]: isOpen }, [additionalClassName])}>
       <AppLink to={pagesPaths.main}>
         <HomeIcon className={css.icon} />
-        <span className={css.text}>Главная страница</span>
+        <span className={css.text}>{t("main_page")}</span>
       </AppLink>
       <AppLink to={pagesPaths.about}>
         <ContactSupportIcon className={css.icon} />
-        <span className={css.text}>О сайте</span>
+        <span className={css.text}>{t("about_page")}</span>
       </AppLink>
       <ThemeSwitcher />
       <LanguageSwitcher />
