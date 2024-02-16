65c05b6dce899a606cdf77e65e8279b769ac3acf
Spiryaev <kaline-son@ya.ru>
2024-02-16 21:59:59 +0700

    fix: loading

--- src/2entities/PprTable/ui/PprTable.tsx
+++ src/2entities/PprTable/ui/PprTable.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
-import { Table } from "@/1shared/ui/table/ui/Table";
-import { TableCell } from "@/1shared/ui/table/ui/TableCell";
+import { Table } from "@/1shared/ui/Table/ui/Table";
+import { TableCell } from "@/1shared/ui/Table/ui/TableCell";
 import { fullColumnsList } from "../model/pprTableSettings";
 import { IPprData } from "..";
 
@@ -8,8 +8,8 @@ interface IPprTableProps {
   data: IPprData[];
 }
 
-export const PprTable: FC<IPprTableProps> = async ({ data }) => {
-    return (
+export const PprTable: FC<IPprTableProps> = ({ data }) => {
+  return (
     <Table
       className="table-fixed w-[120%] [font-size:12px]"
       RowComponent={(props) => <tr {...props}></tr>}
