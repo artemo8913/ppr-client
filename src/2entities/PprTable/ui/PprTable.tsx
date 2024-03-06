c9f0df94677b60688bfa97dc7fc9438fe5cc8d50
Spiryaev <kaline-son@ya.ru>
2024-03-06 15:14:04 +0700

    feat: add ppr table data provider

--- src/2entities/PprTable/ui/PprTable.tsx
+++ src/2entities/PprTable/ui/PprTable.tsx
@@ -3,7 +3,7 @@ import { FC, useEffect } from "react";
 import { IPpr, IPprData } from "@/1shared/api/pprTable";
 import { TableCell, Table } from "@/1shared/ui/table";
 import { fullColumnsList } from "../lib/pprTableSettings";
-import { PprTableDataProvider, usePprTableData } from "../model/PprTableDataProvider";
+import { usePprTableData } from "../model/PprTableDataProvider";
 
 interface IPprTableProps {}
 
