ae03fdf25276160674d8db220e9b68ec4563ec46
Spiryaev <kaline-son@ya.ru>
2024-03-06 11:08:16 +0700

    feat: ppr table upd

--- src/2entities/PprTable/api/pprTable.actions.ts
+++ src/2entities/PprTable/api/pprTable.actions.ts
@@ -18,6 +18,15 @@ export async function addPprTable(id: string) {
   const responce: Promise<IPpr> = await query.json();
   return responce;
 }
+export async function updatePprTable(id: string, params: Partial<Omit<IPpr, "id">>) {
+  const query = await fetch(`${PPR_API_URL}/${id}`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(params),
+  });
+  const responce: Promise<IPpr> = await query.json();
+  return responce;
+}
 export async function deletePprTable(id: string) {
   const query = await fetch(`${PPR_API_URL}/${id}`, {
     method: "DELETE",
