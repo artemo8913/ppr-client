a1f449386c6b7f41c2c39dfb1ff6b06914b747c1
Spiryaev <kaline-son@ya.ru>
2024-03-16 21:09:09 +0700

    fix: delete antd registry

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,9 @@
 import { LoginForm } from "@/3features/login";
-import { AntdRegistry } from "@ant-design/nextjs-registry";
 
 export default function LoginPage() {
   return (
-    <AntdRegistry>
-      <div className="h-screen flex justify-center">
-        <LoginForm />
-      </div>
-    </AntdRegistry>
+    <div className="h-screen flex justify-center">
+      <LoginForm />
+    </div>
   );
 }
