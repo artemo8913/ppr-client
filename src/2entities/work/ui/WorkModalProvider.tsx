b11a69857d223e59a73f49de0678a07ed05b3e2f
Spiryaev Artem <kaline-son@ya.ru>
2024-03-18 19:55:18 +0700

    feat: add interactive modal for adding new works

---
+++ src/2entities/work/ui/WorkModalProvider.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { FC, PropsWithChildren, createContext, useCallback, useContext, useState } from "react";
+
+interface IWorkModalControlProps {
+  isOpen: boolean;
+  openModal: () => void;
+  closeModal: () => void;
+}
+const defaultValue: boolean = false;
+
+const WorkDataContext = createContext<IWorkModalControlProps>({
+  isOpen: defaultValue,
+  openModal: () => {},
+  closeModal: () => {},
+});
+
+interface IWorkModalProviderProps extends PropsWithChildren {}
+
+export const WorkModalProvider: FC<IWorkModalProviderProps> = ({ children }) => {
+  const [isOpen, setIsModalOpen] = useState(false);
+  const openModal = useCallback(() => setIsModalOpen(true), []);
+  const closeModal = useCallback(() => setIsModalOpen(false), []);
+  return <WorkDataContext.Provider value={{ isOpen, closeModal, openModal }}>{children}</WorkDataContext.Provider>;
+};
+
+export const useWorkModal = () => useContext(WorkDataContext);
--- src/app/ppr/[id]/page.tsx
+++ src/app/ppr/[id]/page.tsx
@@ -1,16 +1,22 @@
 import { getPprTable } from "@/1shared/api/pprTable";
+import { getAllWorks } from "@/1shared/api/work";
+import { WorkModal, WorkModalProvider } from "@/2entities/work";
 import { PprTable, PprTableDataProvider } from "@/4widgets/pprTable";
-import { PprTableUpdateButton } from "@/3features/pprTableUpdate";
 import { PprTableControlPanel } from "@/4widgets/pprTable/ui/PprTableControlPanel";
 
 export default async function PprPageId({ params }: { params: { id: string } }) {
   const ppr = await getPprTable(params.id);
+  const works = await getAllWorks();
+
   return (
     <PprTableDataProvider ppr={ppr}>
-      <div className="w-full h-full overflow-scroll">
-        <PprTableControlPanel id={params.id} />
-        <PprTable />
-      </div>
+      <WorkModalProvider>
+        <div className="w-full h-full overflow-scroll">
+          <PprTableControlPanel id={params.id} />
+          <PprTable />
+        </div>
+        <WorkModal data={works} />
+      </WorkModalProvider>
     </PprTableDataProvider>
   );
 }
