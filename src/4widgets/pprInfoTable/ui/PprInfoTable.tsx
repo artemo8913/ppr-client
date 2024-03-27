1935200b81291bb59697cbe491c822d94b53bfa9
Spiryaev <kaline-son@ya.ru>
2024-03-27 21:06:38 +0700

    feat: add status widget in ppr info table

--- src/4widgets/pprInfoTable/ui/PprInfoTable.tsx
+++ src/4widgets/pprInfoTable/ui/PprInfoTable.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { FC } from "react";
 import Link from "next/link";
-import { Table, TableProps } from "antd";
+import { Badge, Table, TableProps } from "antd";
 import { DeleteTwoTone } from "@ant-design/icons";
 import { directions } from "@/1shared/types/transEnergoDivisions";
 import { IPpr, deletePprTable } from "@/1shared/api/pprTable";
 import { PprDeleteButton } from "@/3features/pprDeleteTable";
+import { months, monthsIntlRu } from "@/1shared/types/date";
 
 interface IPprInfoProps {
   data: IPpr[];
@@ -54,6 +55,43 @@ const columns: TableProps<IPpr>["columns"] = [
         return "-";
       }
       return directions[data.id_direction].short_name;
+    },
+  },
+  {
+    title: "Статус",
+    dataIndex: "status",
+    key: "status",
+  },
+  {
+    title: "Месяцы",
+    dataIndex: "months_statuses",
+    key: "months_statuses",
+    render: (_, data) => {
+      return (
+        <div className="flex flex-row">
+          {months.map((month) => (
+            <div className="flex min-w-3 flex-col justify-center items-center" key={month}>
+              <span>{monthsIntlRu[month][0]}</span>
+              <Badge
+                key={month}
+                status={
+                  data.months_statuses[month] === "none"
+                    ? "default"
+                    : data.months_statuses[month] === "done"
+                    ? "success"
+                    : data.months_statuses[month] === "fact_filling"
+                    ? "processing"
+                    : data.months_statuses[month] === "plan_on_correction"
+                    ? "error"
+                    : data.months_statuses[month] === "plan_aproved"
+                    ? "processing"
+                    : "warning"
+                }
+              />
+            </div>
+          ))}
+        </div>
+      );
     },
   },
   {
