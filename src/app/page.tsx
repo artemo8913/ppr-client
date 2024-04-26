f75c610f7beb9f1c20f7f26e95fd338c3d081379
Spiryaev <kaline-son@ya.ru>
2024-04-26 07:54:38 +0700

    feat: create antd client table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import PeoplesTable from "@/2entities/peoples/ui/PeoplesTable";
+
 export default async function Home() {
-  return <main>asd</main>;
+  return (
+    <main>
+      <PeoplesTable />
+    </main>
+  );
 }
