c2e6c5ebeaa46b8b7388a07bd5d50fd8261d8f28
Spiryaev Artem <kaline-son@ya.ru>
2024-04-30 13:35:33 +0700

    fix: imports

--- src/1shared/providers/pprTableProvider/index.ts
+++ src/1shared/providers/pprTableProvider/index.ts
@@ -1,1 +1,4 @@
+export type { TFilterMonthsOption, TFilterPlanFactOption } from "./model/PprTableSettingsProvider";
+
 export { PprTableDataProvider, usePprTableData } from "./model/PprTableDataProvider";
+export { PprTableSettingsProvider, usePprTableSettings } from "./model/PprTableSettingsProvider";
