efc23db2dde96aea1dad5ecd8c886ca2bf5b5d37
Spiryaev Artem <kaline-son@ya.ru>
2024-05-19 13:28:33 +0700

    fix: remove function isPprInUserControl

--- src/1shared/providers/pprTableProvider/index.ts
+++ src/1shared/providers/pprTableProvider/index.ts
@@ -5,9 +5,5 @@ export type {
 } from "./model/PprTableViewSettingsProvider";
 export { PprTableDataProvider, usePprTableData } from "./model/PprTableDataProvider";
 export { PprTableViewSettingsProvider, usePprTableViewSettings } from "./model/PprTableViewSettingsProvider";
-export {
-  getNextPprMonthStatus,
-  getNextPprYearStatus,
-  isAllMonthsPprStatusesIsDone,
-  isPprInUserControl,
-} from "./lib/pprStatusHelper";
+export { getNextPprMonthStatus, getNextPprYearStatus, isAllMonthsPprStatusesIsDone } from "./lib/pprStatusHelper";
+export { isPprInUserControl } from "./lib/isPprInUserControl";
--- src/1shared/providers/pprTableProvider/lib/pprStatusHelper.ts
+++ src/1shared/providers/pprTableProvider/lib/pprStatusHelper.ts
@@ -41,31 +41,6 @@ const nextPprMonthStatus: { [key in TMonthPprStatus]?: TMonthPprStatus | undefin
   fact_on_agreement_sub_boss: "done",
   done: "plan_on_correction",
 };
-/**Подходит ли данный ППР для определенных ролей (начальника цеха, инженера, заместителя начальника дистанции, начальника дистанции и т.п.) */
-export function isPprInUserControl(ppr: IPpr, userData: IUser) {
-  const { created_by: ppr_created_by } = ppr;
-  const {
-    id_distance: user_id_distance,
-    id_subdivision: user_id_subdivision,
-    id_direction: user_id_direction,
-    role: user_role,
-  } = userData;
-
-  // Проверка принадлежности ППР к подразделению/дистанции/дирекции
-  const isMySubdivision = ppr_created_by?.id_subdivision === user_id_subdivision;
-  const isMyDistance = ppr_created_by?.id_distance === user_id_distance;
-  const isMyDirection = ppr_created_by?.id_direction === user_id_direction;
-
-  // Проверка соответствия роли и подразделения/дистанции/дирекции
-  const isForSubdivision = user_role === "subdivision" && isMySubdivision && isMyDistance && isMyDirection;
-  const isForEngineer = user_role === "distance_engineer" && isMyDistance && isMyDirection;
-  const isForTimeNorm = user_role === "distance_time_norm" && isMyDistance && isMyDirection;
-  const isForSecurityEngineer = user_role === "distance_security_engineer" && isMyDistance && isMyDirection;
-  const isForSubBoss = user_role === "distance_sub_boss" && isMyDistance && isMyDirection;
-  const isForBoss = user_role === "distance_boss" && isMyDistance && isMyDirection;
-
-  return { isForSubdivision, isForEngineer, isForTimeNorm, isForSecurityEngineer, isForSubBoss, isForBoss };
-}
 
 export function getNextPprYearStatus(currentStatus: TYearPprStatus): TYearPprStatus | undefined {
   return nextPprYearStatus[currentStatus];
