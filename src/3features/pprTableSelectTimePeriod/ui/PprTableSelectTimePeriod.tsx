02179450ea0bf8743d0144d56f04a819874812bf
Spiryaev Artem <kaline-son@ya.ru>
2024-05-19 10:04:54 +0700

    fix: build (ppr table control panel cant use useEffect)

--- src/3features/pprTableSelectTimePeriod/ui/PprTableSelectTimePeriod.tsx
+++ src/3features/pprTableSelectTimePeriod/ui/PprTableSelectTimePeriod.tsx
@@ -1,14 +1,21 @@
 "use client";
 import Select from "antd/es/select";
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { TPprTimePeriod, monthsIntlRu, pprTimePeriods } from "@/1shared/types/date";
-import { usePprTableViewSettings } from "@/1shared/providers/pprTableProvider";
+import { usePprTableData, usePprTableViewSettings } from "@/1shared/providers/pprTableProvider";
+import { findPossibleCurrentPprPeriod } from "@/1shared/providers/pprTableProvider/lib/findPossibleCurrentPprPeriod";
 
 interface IPprTableSelectTimePeriodProps {}
 
 export const PprTableSelectTimePeriod: FC<IPprTableSelectTimePeriodProps> = () => {
   const { currentTimePeriod, setTimePeriod } = usePprTableViewSettings();
-  
+  const { pprData } = usePprTableData();
+
+  useEffect(() => {
+    setTimePeriod(findPossibleCurrentPprPeriod(pprData));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Select<TPprTimePeriod>
       className="min-w-24"
--- src/4widgets/pprTableControlPanel/ui/PprTableControlPanel.tsx
+++ src/4widgets/pprTableControlPanel/ui/PprTableControlPanel.tsx
@@ -1,6 +1,4 @@
-import { FC, useEffect } from "react";
-import { findPossibleCurrentPprPeriod } from "@/1shared/providers/pprTableProvider/lib/findPossibleCurrentPprPeriod";
-import { usePprTableViewSettings } from "@/1shared/providers/pprTableProvider";
+import { FC } from "react";
 import { IPpr } from "@/2entities/pprTable";
 import { PprTableSaveButton } from "@/3features/pprTableSave";
 import { PprTableYearStatusUpdate } from "@/3features/pprTableStatusUpdate";
@@ -14,13 +12,6 @@ interface IPprTableControlPanelProps {
 }
 
 export const PprTableControlPanel: FC<IPprTableControlPanelProps> = ({ pprData }) => {
-  const { setTimePeriod } = usePprTableViewSettings();
-
-  useEffect(() => {
-    setTimePeriod(findPossibleCurrentPprPeriod(pprData));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  
   return (
     <div className="flex justify-start items-center sticky top-0 left-0 z-10 bg-slate-300">
       Статус: {pprData?.status} Создан: {new Date(pprData?.created_at!).toLocaleDateString()} Год: {pprData?.year}{" "}
