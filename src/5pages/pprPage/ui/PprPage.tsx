69c68e3088c60976fe5a66a82602995df405342f
Spiryaev <kaline-son@ya.ru>
2024-06-08 16:39:52 +0700

    feat: create ppr page

---
+++ src/5pages/pprPage/ui/PprPage.tsx
@@ -0,0 +1,56 @@
+"use client";
+import Tabs from "antd/es/tabs";
+import { WorkingManAdd } from "@/3features/pprWorkingMans/add";
+import { PprTable } from "@/4widgets/pprTable";
+import { WorkingMansTable } from "@/4widgets/workingMansTable";
+import { CorrectionRaport } from "@/4widgets/correctionRaport";
+import { MonthPlan } from "@/4widgets/monthPlan";
+import { usePprTableSettings } from "@/1shared/providers/pprTableSettingsProvider";
+
+export const PprPage = () => {
+  const { currentTimePeriod } = usePprTableSettings();
+
+  return (
+    <Tabs
+      defaultActiveKey="2"
+      items={[
+        {
+          key: "1",
+          label: "Настой часов",
+          children: (
+            <div className="h-[80vh] overflow-auto">
+              <WorkingManAdd className="mb-2" />
+              <WorkingMansTable />
+            </div>
+          ),
+        },
+        {
+          key: "2",
+          label: "План ТОиР",
+          children: (
+            // TODO #16 Переписать на свой компонент табы. Здесь костыль с высотой
+            <div className="h-[80vh] overflow-auto">
+              <PprTable />
+            </div>
+          ),
+        },
+        {
+          key: "3",
+          label: "Месячный план",
+          disabled: currentTimePeriod === "year",
+          children: (
+            <div className="h-[80vh] overflow-auto">
+              <MonthPlan />
+            </div>
+          ),
+        },
+        {
+          key: "4",
+          disabled: currentTimePeriod === "year",
+          label: "Рапорт на корректировку плана",
+          children: <CorrectionRaport />,
+        },
+      ]}
+    />
+  );
+};
--- src/app/ppr/[id]/page.tsx
+++ src/app/ppr/[id]/page.tsx
@@ -1,16 +1,11 @@
-import Tabs from "antd/es/tabs";
 import { PprProvider } from "@/1shared/providers/pprProvider";
 import { PprTableSettingsProvider } from "@/1shared/providers/pprTableSettingsProvider";
 import { WorkModalProvider } from "@/1shared/providers/workModalProvider";
 import { getPprTable } from "@/2entities/ppr";
 import { getAllWorks } from "@/2entities/work";
-import { WorkingManAdd } from "@/3features/pprWorkingMans/add";
 import { WorkModal } from "@/4widgets/workModal";
-import { PprTable } from "@/4widgets/pprTable";
 import { PprTableControlPanel } from "@/4widgets/pprTableControlPanel";
-import { WorkingMansTable } from "@/4widgets/workingMansTable";
-import { CorrectionRaport } from "@/4widgets/correctionRaport";
-import { MonthPlan } from "@/4widgets/monthPlan";
+import { PprPage } from "@/5pages/pprPage";
 
 export default async function PprPageId({ params }: { params: { id: string } }) {
   const ppr = await getPprTable(params.id);
@@ -23,45 +18,7 @@ export default async function PprPageId({ params }: { params: { id: string } }) 
       <WorkModalProvider>
         <PprProvider pprFromResponce={ppr}>
           <PprTableControlPanel ppr={ppr} />
-          <Tabs
-            defaultActiveKey="2"
-            items={[
-              {
-                key: "1",
-                label: "Настой часов",
-                children: (
-                  <div className="h-[80vh] overflow-auto">
-                    <WorkingManAdd className="mb-2" />
-                    <WorkingMansTable />
-                  </div>
-                ),
-              },
-              {
-                key: "2",
-                label: "План ТОиР",
-                children: (
-                  // TODO #16 Переписать на свой компонент табы. Здесь костыль с высотой
-                  <div className="h-[80vh] overflow-auto">
-                    <PprTable />
-                  </div>
-                ),
-              },
-              {
-                key: "3",
-                label: "Месячный план",
-                children: (
-                  <div className="h-[80vh] overflow-auto">
-                    <MonthPlan />
-                  </div>
-                ),
-              },
-              {
-                key: "4",
-                label: "Рапорт на корректировку плана",
-                children: <CorrectionRaport />,
-              },
-            ]}
-          />
+          <PprPage />
           <WorkModal data={works} />
         </PprProvider>
       </WorkModalProvider>
