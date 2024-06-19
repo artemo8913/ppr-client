ed6dbe52bb4ce41e61912fd9fda1d944b47096c0
Spiryaev <kaline-son@ya.ru>
2024-06-19 07:32:23 +0700

    feat: update working man table and create working man table cell

---
+++ src/4widgets/workingMansTable/ui/WorkingManTableCell.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { FC, memo, useCallback } from "react";
+import { ITableCellProps, TableCell } from "@/1shared/ui/table";
+import { IWorkingManYearPlan } from "@/2entities/ppr";
+
+interface IWorkingManTableCellProps extends ITableCellProps {
+  rowIndex?: number;
+  field?: keyof IWorkingManYearPlan;
+  updateWorkingManTableCell?: (rowIndex: number, field: keyof IWorkingManYearPlan, value: unknown) => void;
+}
+
+const WorkingManTableCell: FC<IWorkingManTableCellProps> = ({
+  rowIndex,
+  field,
+  updateWorkingManTableCell,
+  ...otherProps
+}) => {
+  const handleChange = useCallback(
+    (value: unknown) => {
+      if (rowIndex === undefined || !field || !updateWorkingManTableCell) {
+        return;
+      }
+      updateWorkingManTableCell(rowIndex, field, value);
+    },
+    [rowIndex, field, updateWorkingManTableCell]
+  );
+
+  return <TableCell {...otherProps} handleBlur={handleChange} />;
+};
+
+const WorkingManTableCellMemo = memo(WorkingManTableCell);
+
+export { WorkingManTableCell, WorkingManTableCellMemo };
