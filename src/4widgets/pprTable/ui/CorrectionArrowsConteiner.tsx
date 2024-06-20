fbfa3b66a60edd0c2a7169eda896b6b80016116e
Spiryaev <kaline-son@ya.ru>
2024-06-20 10:23:16 +0700

    upd: arrow conteiner

--- src/4widgets/pprTable/ui/CorrectionArrowsConteiner.tsx
+++ src/4widgets/pprTable/ui/CorrectionArrowsConteiner.tsx
@@ -2,13 +2,12 @@
 import { FC, MutableRefObject, memo, useEffect, useState } from "react";
 import { TFilterPlanFactOption, usePprTableSettings } from "@/1shared/providers/pprTableSettingsProvider";
 import { Arrow } from "@/1shared/ui/arrow";
-import { IPlanWorkPeriods, checkIsPlanWorkField, planWorkFields } from "@/2entities/ppr";
-import { usePpr } from "@/1shared/providers/pprProvider";
+import { IPlanWorkPeriods, TTransfer, checkIsPlanWorkField, planWorkFields } from "@/2entities/ppr";
 
 interface ICorrectionArrowsConteinerProps {
   planCellRef: MutableRefObject<HTMLTableCellElement | null>;
-  objectId: string;
-  fieldFrom: keyof IPlanWorkPeriods;
+  field: keyof IPlanWorkPeriods;
+  transfers: TTransfer<IPlanWorkPeriods>[];
 }
 
 function getArrowWidthFactor(planFactFilter: TFilterPlanFactOption): number {
@@ -26,22 +25,16 @@ function getArrowWidthFactor(planFactFilter: TFilterPlanFactOption): number {
   return timePeriodCount * planFactFieldCount;
 }
 
-const CorrectionArrowsConteiner: FC<ICorrectionArrowsConteinerProps> = ({ objectId, planCellRef, fieldFrom }) => {
+const CorrectionArrowsConteiner: FC<ICorrectionArrowsConteinerProps> = ({ planCellRef, field, transfers }) => {
   const [basicArrowWidth, setBasicArrowWidth] = useState(0);
-  const { getWorkTransfers } = usePpr();
   const { filterColumns } = usePprTableSettings();
-  const fieldFromIndex = checkIsPlanWorkField(fieldFrom) ? planWorkFields.indexOf(fieldFrom) : undefined;
+  const fieldFromIndex = checkIsPlanWorkField(field) ? planWorkFields.indexOf(field) : undefined;
 
   useEffect(() => {
     const width = planCellRef.current?.getBoundingClientRect().width || 0;
     const widthFactor = getArrowWidthFactor(filterColumns.planFact);
     setBasicArrowWidth(width * widthFactor);
   }, [filterColumns, planCellRef]);
-
-  const planTransfers = getWorkTransfers("plan", objectId, fieldFrom);
-  const undoneTransfers = getWorkTransfers("undone", objectId, fieldFrom);
-
-  const transfers = [...(planTransfers || []), ...(undoneTransfers || [])];
 
   const arrows = transfers?.map((field, index) => {
     const fieldToIndex = checkIsPlanWorkField(field.fieldTo) ? planWorkFields.indexOf(field.fieldTo) : undefined;
