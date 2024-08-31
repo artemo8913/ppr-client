ac42b0802455c3fd91491fb9789eeaeb3a24f6a8
Spiryaev Artem <kaline-son@ya.ru>
2024-08-31 11:23:54 +0700

    feat: create const for url and api

---
+++ src/1shared/const/url.ts
@@ -0,0 +1,9 @@
+//API
+export const API_LOGIN = "/login";
+export const API_PPR = "/ppr";
+export const API_ALL_PPR = "/all_pprs";
+export const API_USER = "/user";
+export const API_WORK = "/work";
+
+//URL
+export const URL_BASIC = process.env.NEXT_PUBLIC_API_DEV;
