e73ee132eded188e176abcf31cf10af5640d0db4
Spiryaev Artem <kaline-son@ya.ru>
2024-09-21 08:02:05 +0700

    feat: use updatePprTableCell from Provider, use branches Meta for create title rows

--- src/4widgets/pprTable/ui/PprTable.tsx
+++ src/4widgets/pprTable/ui/PprTable.tsx
@@ -1,41 +1,26 @@
 "use client";
-import { FC, useCallback, useRef } from "react";
+import { FC, Fragment, useCallback, useRef } from "react";
 
 import { usePpr } from "@/1shared/providers/pprProvider";
 import { translateRuTimePeriod } from "@/1shared/lib/date";
+import { translateRuPprBranchName } from "@/1shared/locale/pprBranches";
 import { usePprTableSettings } from "@/1shared/providers/pprTableSettingsProvider";
-import {
-  IPprData,
-  SummaryTableFoot,
-  TAllMonthStatuses,
-  TYearPprStatus,
-  checkIsFactTimeField,
-  checkIsFactWorkField,
-  checkIsPlanWorkField,
-  checkIsWorkOrTimeField,
-} from "@/2entities/ppr";
+import { checkIsWorkOrTimeField, IPprData, SummaryTableFoot, TAllMonthStatuses, TYearPprStatus } from "@/2entities/ppr";
 import { AddWorkButton } from "@/3features/ppr/worksUpdate";
 
-import { PprTableCellMemo } from "./PprTableCell";
-import { useCreateColumns } from "./PprTableColumns";
 import HeaderCell from "./HeaderCell";
+import { useCreateColumns } from "./PprTableColumns";
 import { getColumnSettings, getThStyle } from "../lib/pprTableStylesHelper";
 import { translateRuFieldName } from "../lib/pprTableColumnsHelper";
+import { PprTableCellMemo } from "./PprTableCell";
+import { PprTableBranchNameRowMemo } from "./PprTableBranchNameRow";
 
 interface IPprTableProps {}
 
 export const PprTable: FC<IPprTableProps> = ({}) => {
-  const {
-    ppr,
-    updateFactWorkTime,
-    updateFactWork,
-    updatePlanWorkValueByUser,
-    updateNormOfTime,
-    updatePlanWork,
-    updatePprData,
-  } = usePpr();
+  const { ppr, updatePprTableCell, getBranchesMeta } = usePpr();
 
-  const { basicColumns, timePeriods, planFactColumns, monthColSpan } = useCreateColumns();
+  const { basicFields, timePeriods, planFactFields, monthColSpan, allFields } = useCreateColumns();
 
   const pprSettings = usePprTableSettings();
 
@@ -43,25 +28,6 @@ export const PprTable: FC<IPprTableProps> = ({}) => {
   const pprMonthsStatuses: TAllMonthStatuses | null = ppr?.months_statuses || null;
 
   const planCellRef = useRef<HTMLTableCellElement | null>(null);
-
-  const updatePprTableCell = useCallback(
-    (id: string, field: keyof IPprData, value: string, isWorkAproved?: boolean) => {
-      if (field === "norm_of_time") {
-        updateNormOfTime(id, Number(value));
-      } else if (checkIsFactWorkField(field)) {
-        updateFactWork(id, field, Number(value));
-      } else if (checkIsFactTimeField(field)) {
-        updateFactWorkTime(id, field, Number(value));
-      } else if (!isWorkAproved && checkIsPlanWorkField(field)) {
-        updatePlanWork(id, field, Number(value));
-      } else if (isWorkAproved && checkIsPlanWorkField(field)) {
-        updatePlanWorkValueByUser(id, field, Number(value));
-      } else {
-        updatePprData(id, field, value);
-      }
-    },
-    [updateFactWork, updateFactWorkTime, updateNormOfTime, updatePlanWork, updatePlanWorkValueByUser, updatePprData]
-  );
 
   const getColumnSettingsForField = useCallback(
     (field: keyof IPprData, isHaveWorkId: boolean) =>
@@ -76,52 +42,62 @@ export const PprTable: FC<IPprTableProps> = ({}) => {
     [pprMonthsStatuses, pprSettings.correctionView, pprSettings.currentTimePeriod, pprYearStatus]
   );
 
+  if (!Boolean(ppr?.data.length)) {
+    return <AddWorkButton shape="default" size="middle" type="primary" label="Добавить работу" />;
+  }
+
+  const { branchesAndSubbrunchesOrder } = getBranchesMeta();
+
   return (
-    <>
-      {!Boolean(ppr?.data.length) && (
-        <AddWorkButton shape="default" size="middle" type="primary" label="Добавить работу" />
-      )}
-      <table
-        style={{
-          tableLayout: "fixed",
-          width: `${pprSettings.tableWidthPercent}%`,
-          fontSize: `${pprSettings.fontSizePx}px`,
-          position: "relative",
-        }}
-      >
-        <thead>
-          <tr>
-            {basicColumns.map((field) => (
-              <HeaderCell
-                isVertical
-                key={field}
-                rowSpan={2}
-                value={translateRuFieldName(field)}
-                style={{
-                  ...getThStyle(field),
-                  height: `${pprSettings.headerHeightPx}px`,
-                }}
-              />
-            ))}
-            {timePeriods.map((month) => (
-              <HeaderCell key={month} colSpan={monthColSpan} value={translateRuTimePeriod(month)} />
-            ))}
-          </tr>
-          <tr>
-            {planFactColumns.map((planFactField) => (
-              <HeaderCell
-                isVertical
-                key={planFactField}
-                value={translateRuFieldName(planFactField)}
-                cellRef={planFactField === "year_plan_work" ? planCellRef : null}
-              />
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {ppr?.data.map((pprData) => (
+    <table
+      style={{
+        tableLayout: "fixed",
+        width: `${pprSettings.tableWidthPercent}%`,
+        fontSize: `${pprSettings.fontSizePx}px`,
+        position: "relative",
+      }}
+    >
+      <thead>
+        <tr>
+          {basicFields.map((field) => (
+            <HeaderCell
+              isVertical
+              key={field}
+              rowSpan={2}
+              value={translateRuFieldName(field)}
+              style={{
+                ...getThStyle(field),
+                height: `${pprSettings.headerHeightPx}px`,
+              }}
+            />
+          ))}
+          {timePeriods.map((month) => (
+            <HeaderCell key={month} colSpan={monthColSpan} value={translateRuTimePeriod(month)} />
+          ))}
+        </tr>
+        <tr>
+          {planFactFields.map((planFactField) => (
+            <HeaderCell
+              isVertical
+              key={planFactField}
+              value={translateRuFieldName(planFactField)}
+              cellRef={planFactField === "year_plan_work" ? planCellRef : null}
+            />
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {ppr?.data.map((pprData, index) => (
+          <Fragment key={pprData.id}>
+            {index in branchesAndSubbrunchesOrder &&
+              branchesAndSubbrunchesOrder[index].map((branch) => (
+                <PprTableBranchNameRowMemo
+                  key={branch.name}
+                  label={`${branch.orderIndex} ${translateRuPprBranchName(branch.name)}`}
+                />
+              ))}
             <tr key={pprData.id}>
-              {basicColumns.concat(planFactColumns).map((field) => (
+              {allFields.map((field) => (
                 <PprTableCellMemo
                   key={pprData.id + field}
                   pprData={pprData}
@@ -133,14 +109,14 @@ export const PprTable: FC<IPprTableProps> = ({}) => {
                 />
               ))}
             </tr>
-          ))}
-        </tbody>
-        <SummaryTableFoot
-          fields={planFactColumns}
-          summaryNameColSpan={basicColumns.length}
-          totalFieldsValues={ppr?.total_fields_value}
-        />
-      </table>
-    </>
+          </Fragment>
+        ))}
+      </tbody>
+      <SummaryTableFoot
+        fields={planFactFields}
+        summaryNameColSpan={basicFields.length}
+        totalFieldsValues={ppr?.total_fields_value}
+      />
+    </table>
   );
 };
