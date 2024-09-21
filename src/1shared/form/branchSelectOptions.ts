815360ceaa3222f79a5fab58ca64c0bb212a23fa
Spiryaev Artem <kaline-son@ya.ru>
2024-09-21 07:57:00 +0700

    upd: BRANCH_SELECT_OPTIONS

--- src/1shared/form/branchSelectOptions.ts
+++ src/1shared/form/branchSelectOptions.ts
@@ -1,9 +1,7 @@
 import { TWorkBranch } from "@/2entities/ppr";
 
 export const BRANCH_SELECT_OPTIONS: { value: TWorkBranch; label: string }[] = [
-  { value: "exploitation", label: "Эксплуатационный план" },
+  { value: "exploitation", label: "Основные работы" },
   { value: "additional", label: "Дополнительные работы" },
   { value: "unforeseen", label: "Непредвиденные работы" },
-  { value: "security", label: "Обеспечение работ" },
-  { value: "none", label: "Прочие работы" },
 ];
