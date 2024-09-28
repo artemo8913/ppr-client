a4adf036378b139f0e12ad794604f2af8172a535
Spiryaev Artem <kaline-son@ya.ru>
2024-09-28 12:42:35 +0700

    feat: add drizzle and mysql2

---
+++ database/index.ts
@@ -0,0 +1,12 @@
+import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise";
+
+const poolConnection = mysql.createPool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+const db = drizzle(poolConnection);
+
+export default db;
