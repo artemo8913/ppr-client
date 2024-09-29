053a45d731dca20d9924b5cd90f98c8036a800d5
Spiryaev Artem <kaline-son@ya.ru>
2024-09-29 15:01:58 +0700

    upd: drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,19 @@
+import { cwd } from "node:process";
+import { loadEnvConfig } from "@next/env";
 import { defineConfig } from "drizzle-kit";
 
+loadEnvConfig(cwd());
+
 export default defineConfig({
-  schema: "./src/2entities/**/*.db.ts",
+  schema: "./src/**/*.schema.ts",
   dialect: "mysql",
   out: "./drizzle",
+  dbCredentials: {
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT),
+    host: "localhost",
+    url: "db",
+  },
 });
