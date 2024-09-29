2a816aed4c86046ef293b8ce34c834916b6a8e51
Spiryaev Artem <kaline-son@ya.ru>
2024-09-29 15:12:39 +0700

    feat: upd main page and clue table for login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,24 @@ import { IUser, translateRuUserRole } from "@/2entities/user";
 
 export default async function Home() {
   const user = await getServerSession(authOptions);
+
   if (!user) {
     return null;
   }
 
   const credentials: IUser = user?.user;
-  const { id, id_direction, id_distance, id_subdivision, role } = credentials;
+  const { id, id_direction, id_distance, id_subdivision, role, first_name, middle_name, last_name } = credentials;
+
+  const fullName = `${last_name} ${first_name[0]}.${middle_name[0]}.`;
 
   return (
     <main>
+      <div>id: {id}</div>
       <div>{id_direction ? directionsMock[id_direction].short_name : null}</div>
       <div>{id_distance && id_direction ? directionsMock[id_direction].distances[id_distance].short_name : null}</div>
       <div>ЭЧК-{id_subdivision}</div>
       <div>Роль: {translateRuUserRole(role)}</div>
+      <div>Ф.И.О.: {fullName}</div>
     </main>
   );
 }
