3a8ad2700589a18e536fc148737b17f188972c7e
Spiryaev Artem <kaline-son@ya.ru>
2024-10-10 10:57:06 +0700

    fix: ppr table style

--- src/4widgets/pprTable/ui/PprTable.tsx
+++ src/4widgets/pprTable/ui/PprTable.tsx
@@ -4,12 +4,12 @@ import { FC, Fragment, useCallback, useRef } from "react";
 import { usePpr } from "@/1shared/providers/pprProvider";
 import { translateRuTimePeriod } from "@/1shared/lib/date";
 import { usePprTableSettings } from "@/1shared/providers/pprTableSettingsProvider";
-import { checkIsWorkOrTimeField, IPprData, SummaryTableFoot, TAllMonthStatuses, TYearPprStatus } from "@/2entities/ppr";
+import { IPprData, SummaryTableFoot, TAllMonthStatuses, TYearPprStatus } from "@/2entities/ppr";
 import { AddWorkButton } from "@/3features/ppr/worksUpdate";
 
 import HeaderCell from "./HeaderCell";
 import { useCreateColumns } from "./PprTableColumns";
-import { getColumnSettings, getThStyle } from "../lib/pprTableStylesHelper";
+import { checkIsFieldVertical, getColumnSettings, getThStyle } from "../lib/pprTableStylesHelper";
 import { translateRuFieldName } from "../lib/pprTableColumnsHelper";
 import { PprTableCellMemo } from "./PprTableCell";
 import { PprTableBranchNameRowMemo } from "./PprTableBranchNameRow";
@@ -102,7 +102,7 @@ export const PprTable: FC<IPprTableProps> = () => {
                   key={pprData.id + field}
                   pprData={pprData}
                   updatePprTableCell={updatePprTableCell}
-                  isVertical={checkIsWorkOrTimeField(field)}
+                  isVertical={checkIsFieldVertical(field)}
                   field={field}
                   planCellRef={planCellRef}
                   {...getColumnSettingsForField(field, pprData.common_work_id !== null)}
