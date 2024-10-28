b6e3224645357ac1ca53ca624e875d4f8421a2ab
Spiryaev Artem <kaline-son@ya.ru>
2024-10-29 03:55:42 +0700

    feat: use unstable_cache for divisions db request

--- src/app/ppr/page.tsx
+++ src/app/ppr/page.tsx
@@ -5,24 +5,38 @@ import { getAllDirections, getAllDistances, getAllSubdivision } from "@/2entitie
 import { PprSearchQuery } from "@/3features/ppr/search";
 import { PprInfoTable } from "@/4widgets/pprShortInfoTable";
 import { CreatePprModal } from "@/4widgets/createPprModal";
+import { unstable_cache } from "next/cache";
 
 interface IPprPageProps {
   searchParams: { [key in keyof TPprShortInfo]?: string };
 }
 
+const getDivisions = unstable_cache(
+  async () => {
+    const [subdivisions, distances, directions] = await Promise.all([
+      getAllSubdivision(),
+      getAllDistances(),
+      getAllDirections(),
+    ]).catch((e) => {
+      throw new Error(`Load divisions data for ppr info page error. ${e}`);
+    });
+
+    return { subdivisions, distances, directions };
+  },
+  ["divisions"],
+  { revalidate: 3600, tags: ["divisions"] }
+);
+
 export default async function PprPage({ searchParams }: IPprPageProps) {
-  const [pprs, subdivisions, distances, directions] = await Promise.all([
-    getManyPprsShortInfo({ ...searchParams }),
-    getAllSubdivision(),
-    getAllDistances(),
-    getAllDirections(),
-  ]).catch((e) => {
-    throw new Error(`Load data for ppr info page error. ${e}`);
-  });
+  const [pprs, divisions] = await Promise.all([getManyPprsShortInfo({ ...searchParams }), getDivisions()]).catch(
+    (e) => {
+      throw new Error(`Load data for ppr info page error. ${e}`);
+    }
+  );
 
   return (
     <Card size="small">
-      <PprSearchQuery className="mb-4" divisions={{ subdivisions, distances, directions }} />
+      <PprSearchQuery className="mb-4" divisions={divisions} />
       <PprInfoTable data={pprs} />
       <CreatePprModal className="!block ml-auto" />
     </Card>
