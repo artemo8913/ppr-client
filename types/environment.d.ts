fc3ef3e019c29177df9f31ffa710faee3bf3c21a
Spiryaev Artem <kaline-son@ya.ru>
2024-10-30 11:56:36 +0700

    feat: add env variable

--- types/environment.d.ts
+++ types/environment.d.ts
@@ -4,6 +4,7 @@ declare global {
       VERCEL?: "1";
       NEXTAUTH_URL?: string;
       NEXTAUTH_SECRET?: string;
+      ENVIRONMENT?: "PROD" | "DEV";
       DB_NAME?: string;
       DB_USER?: string;
       DB_PASSWORD?: string;
