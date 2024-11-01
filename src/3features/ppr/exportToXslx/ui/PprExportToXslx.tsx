526d97f5e8f07987554baf3b2db23bb18cf8fac8
Spiryaev Artem <kaline-son@ya.ru>
2024-11-01 15:18:28 +0700

    fix: PprExportToXslx btn

--- src/3features/ppr/exportToXslx/ui/PprExportToXslx.tsx
+++ src/3features/ppr/exportToXslx/ui/PprExportToXslx.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FC, useCallback, useRef, useState } from "react";
+import { FC, useCallback, useEffect, useRef, useState } from "react";
 import { Tooltip } from "antd";
 import Button from "antd/es/button";
 import { FileExcelOutlined } from "@ant-design/icons";
@@ -9,15 +9,19 @@ interface IPprExportToXslxProps {}
 export const PprExportToXslx: FC<IPprExportToXslxProps> = () => {
   const linkRef = useRef<HTMLAnchorElement | null>(null);
 
+  const [exportUrl, setExportUrl] = useState("");
+
   const handleClick = useCallback(async () => {
     if (linkRef.current) {
       linkRef.current.click();
     }
   }, []);
 
+  useEffect(() => setExportUrl(`${window?.location}/export`), []);
+
   return (
     <Tooltip title="Экспорт excel">
-      <a ref={linkRef} hidden href={`${window.location}/export`} target="_blank" />
+      <a ref={linkRef} hidden href={exportUrl} target="_blank" />
       <Button icon={<FileExcelOutlined />} type="text" shape="circle" onClick={handleClick} />
     </Tooltip>
   );
