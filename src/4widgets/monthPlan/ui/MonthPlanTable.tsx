4e20f495e37b6fee77729b71d22d029c0a744480
Spiryaev Artem <kaline-son@ya.ru>
2024-12-09 08:46:05 +0700

    upd: month plan table

--- src/4widgets/monthPlan/ui/MonthPlanTable.tsx
+++ src/4widgets/monthPlan/ui/MonthPlanTable.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { FC } from "react";
+import { FC, Fragment } from "react";
 
 import { TTimePeriod } from "@/1shared/const/date";
 import { usePpr } from "@/1shared/providers/pprProvider";
+import { translateRuPprBranchName } from "@/1shared/locale/pprBranches";
 import { usePprTableSettings } from "@/1shared/providers/pprTableSettingsProvider";
 import {
   getFactTimeFieldByTimePeriod,
@@ -15,6 +16,7 @@ import {
 function getMonthPlanFields(timePeriod: TTimePeriod): Array<keyof IPprData> {
   return [
     "name",
+    "location",
     "measure",
     getPlanWorkFieldByTimePeriod(timePeriod),
     "norm_of_time",
@@ -27,22 +29,25 @@ function getMonthPlanFields(timePeriod: TTimePeriod): Array<keyof IPprData> {
 interface IMonthPlanTableProps {}
 
 export const MonthPlanTable: FC<IMonthPlanTableProps> = () => {
-  const { ppr } = usePpr();
+  const { ppr, pprMeta } = usePpr();
 
   const { currentTimePeriod } = usePprTableSettings();
+
+  const columns = getMonthPlanFields(currentTimePeriod);
 
   return (
     <table className="w-full">
       <thead>
         <tr className="*:border *:border-black *:">
           <th rowSpan={2}>№ п.п</th>
-          <th colSpan={2}>Работа</th>
+          <th colSpan={3}>Работа</th>
           <th colSpan={3}>План</th>
           <th colSpan={2}>Выполнение</th>
           <th rowSpan={2}>Примечание</th>
         </tr>
         <tr className="*:border *:border-black">
           <th className="w-2/3">Наименование работ</th>
+          <th>Место работ (тип оборудования)</th>
           <th>Измеритель</th>
           <th>Количество</th>
           <th>Норма времени на измеритель, чел.-ч</th>
@@ -58,24 +63,56 @@ export const MonthPlanTable: FC<IMonthPlanTableProps> = () => {
 
             return Boolean(Number(pprData[`${currentTimePeriod}_fact_work`])) || Boolean(planValue);
           })
-          .map((pprData, rowIndex) => (
-            <tr key={pprData.id}>
-              {/* TODO: Когда научусь считать номера пунктов работ, то буду их писать вместо индекса */}
-              <td className="border border-black text-center">{rowIndex + 1}</td>
-              {getMonthPlanFields(currentTimePeriod).map((field) => {
-                const value = pprData[field];
-                const showValue = value && typeof value === "object" ? value.final : value;
-                const note = field === "name" && pprData.note ? ` (прим. ${pprData.note})` : null;
-
-                return (
-                  <td className="border border-black text-center" key={pprData.id + field}>
-                    {showValue}
-                    {note}
+          .map((pprData, index) => (
+            <Fragment key={pprData.id}>
+              {index in pprMeta.branchesAndSubbrunchesOrder &&
+                "branch" in pprMeta.branchesAndSubbrunchesOrder[index] && (
+                  <tr>
+                    <td colSpan={columns.length} className="border font-bold border-black cursor-default">
+                      {pprMeta.branchesAndSubbrunchesOrder[index].branch!.orderIndex}{" "}
+                      {translateRuPprBranchName(pprMeta.branchesAndSubbrunchesOrder[index].branch!.name)}
+                    </td>
+                  </tr>
+                )}
+              {index in pprMeta.branchesAndSubbrunchesOrder &&
+                "subbranch" in pprMeta.branchesAndSubbrunchesOrder[index] && (
+                  <tr>
+                    <td colSpan={columns.length} className="border font-bold border-black cursor-default">
+                      {pprMeta.branchesAndSubbrunchesOrder[index].subbranch?.orderIndex}{" "}
+                      {pprMeta.branchesAndSubbrunchesOrder[index].subbranch?.name}
+                    </td>
+                  </tr>
+                )}
+              <tr>
+                {/* TODO: Когда научусь считать номера пунктов работ, то буду их писать вместо индекса */}
+                {pprMeta.worksRowSpan[index] === 0 ? null : (
+                  <td rowSpan={pprMeta.worksRowSpan[index]} className="border border-black text-center">
+                    {pprMeta.worksOrderForRowSpan[index]}
                   </td>
-                );
-              })}
-              <td className="border border-black text-center" />
-            </tr>
+                )}
+                {columns.map((field) => {
+                  const value = pprData[field];
+                  const showValue = value && typeof value === "object" ? value.final : value;
+                  const note = field === "name" && pprData.note ? ` (прим. ${pprData.note})` : null;
+
+                  if (pprMeta.worksRowSpan[index] === 0) {
+                    return null;
+                  }
+
+                  return (
+                    <td
+                      rowSpan={pprMeta.worksRowSpan[index]}
+                      className="border border-black text-center"
+                      key={pprData.id + field}
+                    >
+                      {showValue}
+                      {note}
+                    </td>
+                  );
+                })}
+                <td className="border border-black text-center" />
+              </tr>
+            </Fragment>
           ))}
       </tbody>
     </table>
