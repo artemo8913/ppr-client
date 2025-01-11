d1f269cbe70c84f6eb1a1e9f428e9f47525f00cd
Spiryaev Artem <kaline-son@ya.ru>
2025-01-11 16:11:41 +0700

    upd: MainLayoutSider

--- src/4widgets/layouts/ui/MainLayoutSider.tsx
+++ src/4widgets/layouts/ui/MainLayoutSider.tsx
@@ -1,12 +1,12 @@
 "use client";
 import { useState } from "react";
 import { Menu, MenuProps } from "antd";
-import { AuditOutlined, BarChartOutlined, HomeFilled, LogoutOutlined, TableOutlined } from "@ant-design/icons";
+import { BarChartOutlined, HomeFilled, LogoutOutlined, TableOutlined } from "@ant-design/icons";
 import Sider from "antd/es/layout/Sider";
 import Link from "next/link";
 import { signOut } from "next-auth/react";
 
-import { ROUTE_AGREEMENT, ROUTE_PPR, ROUTE_REPORTS, ROUTE_ROOT } from "@/1shared/const/routes";
+import { ROUTE_PPR, ROUTE_REPORTS, ROUTE_ROOT } from "@/1shared/const/routes";
 
 type MenuItem = Required<MenuProps>["items"][number];
 function getItem(label: React.ReactNode, key: React.Key, icon?: React.ReactNode, children?: MenuItem[]): MenuItem {
@@ -21,7 +21,6 @@ function getItem(label: React.ReactNode, key: React.Key, icon?: React.ReactNode,
 const items: MenuItem[] = [
   getItem(<Link href={ROUTE_ROOT}>Главная</Link>, "home", <HomeFilled />),
   getItem(<Link href={ROUTE_PPR}>Годовые планы</Link>, "year", <TableOutlined />),
-  getItem(<Link href={ROUTE_AGREEMENT}>На согласовании / утверждении</Link>, "agreement", <AuditOutlined />),
   getItem(<Link href={ROUTE_REPORTS}>Отчеты</Link>, "reports", <BarChartOutlined />),
   getItem(<a onClick={() => signOut({ callbackUrl: ROUTE_ROOT })}>Выйти</a>, "logout", <LogoutOutlined />),
 ];
