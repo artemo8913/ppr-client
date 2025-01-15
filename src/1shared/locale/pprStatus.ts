8d0a7d534a0890054640ca6d3add24e53abb9a7c
Spiryaev Artem <kaline-son@ya.ru>
2025-01-15 21:44:40 +0700

    fix: ppr status names

--- src/1shared/locale/pprStatus.ts
+++ src/1shared/locale/pprStatus.ts
@@ -3,14 +3,14 @@ import { TMonthPprStatus, TYearPprStatus } from "@/2entities/ppr";
 const MONTH_STATUS_RU: { [status in TMonthPprStatus]: string } = {
   none: "не запланирован",
   plan_creating: "план создаётся",
-  plan_on_agreement_engineer: "на согласовании инженера",
-  plan_on_agreement_time_norm: "на согласовании нормировщика труда",
+  plan_on_agreement_engineer: "на согласовании инженера ПТО",
+  plan_on_agreement_time_norm: "на согласовании инженера по нормированию труда",
   plan_on_aprove: "на утверждении",
   in_process: "план в работе",
   fact_filling: "заполнение выполненных работ",
-  fact_verification_engineer: "проверка объемов выполненных работ",
-  fact_verification_time_norm: "проверка соответствия нормам времени",
-  fact_on_agreement_sub_boss: "проверка выполненных работ",
+  fact_verification_engineer: "проверка выполненных работ инженером ПТО",
+  fact_verification_time_norm: "проверка выполненных работ инженером по нормированию труда",
+  fact_on_agreement_sub_boss: "проверка выполненных работ заместителем начальника",
   done: "завершен",
 };
 
@@ -18,10 +18,10 @@ const YEAR_STATUS_RU: { [status in TYearPprStatus]: string } = {
   done: "Выполнен",
   in_process: "Выполняется",
   plan_creating: "Создается",
-  plan_on_agreement_engineer: "На согласовании отраслевого инженера",
-  plan_on_agreement_sub_boss: "На согласовании отраслевого заместителя дистанции",
+  plan_on_agreement_engineer: "На огласовании инженера ПТО",
+  plan_on_agreement_sub_boss: "На согласовании заместителя начальника дистанции",
   plan_on_agreement_time_norm: "На согласовании инженера по нормированию труда",
-  plan_on_aprove: "На утверждении",
+  plan_on_aprove: "На утверждении заместителем начальника",
   template: "Шаблон",
 };
 
