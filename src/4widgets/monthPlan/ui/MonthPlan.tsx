a3c0ba055d7b8ca32aa0f382b7359fe1b8cbbdac
Spiryaev Artem <kaline-son@ya.ru>
2025-02-04 20:48:11 +0700

    FIX: BUG MONTH PLAN CHANGE YEAR PLAN

--- src/4widgets/monthPlan/ui/MonthPlan.tsx
+++ src/4widgets/monthPlan/ui/MonthPlan.tsx
@@ -5,7 +5,7 @@ import { roundToFixed } from "@/1shared/lib/math";
 import { createPprMeta, usePpr } from "@/1shared/providers/pprProvider";
 import { usePprTableSettings } from "@/1shared/providers/pprTableSettingsProvider";
 import { translateRuTimePeriod } from "@/1shared/locale/date";
-import { getPlanTimeFieldByTimePeriod, getPlanWorkFieldByTimePeriod } from "@/2entities/ppr";
+import { getPlanTimeFieldByTimePeriod, getPlanWorkFieldByTimePeriod, IPprData } from "@/2entities/ppr";
 
 import { MonthPlanTable } from "./MonthPlanTable";
 import { MonthWorkingMansTable } from "./MonthWorkingMansTable";
@@ -34,7 +34,7 @@ export const MonthPlan: FC<IMonthPlanProps> = () => {
       return Boolean(Number(pprData[`${currentTimePeriod}_fact_work`])) || Boolean(planValue);
     })
     .map((pprData) => {
-      const newPprData = { ...pprData };
+      const newPprData: IPprData = JSON.parse(JSON.stringify(pprData));
 
       const planWork = pprData[planWorkField];
 
