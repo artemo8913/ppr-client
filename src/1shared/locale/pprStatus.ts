2544a071d6d5a28e68cb534750f748a20947ec50
Spiryaev Artem <kaline-son@ya.ru>
2025-02-04 12:21:03 +0700

    fix: ppr statuses locale

--- src/1shared/locale/pprStatus.ts
+++ src/1shared/locale/pprStatus.ts
@@ -3,9 +3,9 @@ import { TMonthPprStatus, TYearPprStatus } from "@/2entities/ppr";
 const MONTH_STATUS_RU: { [status in TMonthPprStatus]: string } = {
   none: "не запланирован",
   plan_creating: "план создаётся",
-  plan_on_agreement_engineer: "на согласовании инженера ПТО",
-  plan_on_agreement_time_norm: "на согласовании инженера по нормированию труда",
-  plan_on_aprove: "на утверждении",
+  plan_on_agreement_engineer: "план на согласовании инженера ПТО",
+  plan_on_agreement_time_norm: "план на согласовании инженера по нормированию труда",
+  plan_on_aprove: "план на утверждении",
   in_process: "план в работе",
   fact_filling: "заполнение выполненных работ",
   fact_verification_engineer: "проверка выполненных работ инженером ПТО",
@@ -18,10 +18,10 @@ const YEAR_STATUS_RU: { [status in TYearPprStatus]: string } = {
   done: "Выполнен",
   in_process: "Выполняется",
   plan_creating: "Создается",
-  plan_on_agreement_engineer: "На огласовании инженера ПТО",
-  plan_on_agreement_sub_boss: "На согласовании заместителя начальника дистанции",
+  plan_on_agreement_engineer: "На согласовании инженера ПТО",
   plan_on_agreement_time_norm: "На согласовании инженера по нормированию труда",
-  plan_on_aprove: "На утверждении заместителем начальника",
+  plan_on_agreement_sub_boss: "На согласовании заместителя начальника дистанции",
+  plan_on_aprove: "На утверждении",
   template: "Шаблон",
 };
 
