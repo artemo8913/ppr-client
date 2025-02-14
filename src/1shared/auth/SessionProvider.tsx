62c89b28c331d84dcd1e52546399a936fb05e99c
Spiryaev Artem <kaline-son@ya.ru>
2025-02-14 10:45:48 +0700

    fix: session provider

---
+++ src/1shared/auth/SessionProvider.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { FC, ReactNode } from "react";
+import { SessionProvider as NextAuthSessionProvider } from "next-auth/react";
+
+interface ISessionProviderProps {
+  children: ReactNode;
+}
+
+export const SessionProvider: FC<ISessionProviderProps> = ({ children }) => {
+  return <NextAuthSessionProvider basePath="/api/auth">{children}</NextAuthSessionProvider>;
+};
