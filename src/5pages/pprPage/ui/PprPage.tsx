13f71260c52e7cfda61a3af282a9decd55c0d416
Spiryaev Artem <kaline-son@ya.ru>
2025-03-20 14:26:26 +0700

    fix: antd tab sticky header

--- src/5pages/pprPage/ui/PprPage.tsx
+++ src/5pages/pprPage/ui/PprPage.tsx
@@ -1,12 +1,15 @@
 "use client";
-import { useEffect } from "react";
+import clsx from "clsx";
 import Tabs from "antd/es/tabs";
+import { useEffect } from "react";
 
 import { PprTable } from "@/4widgets/pprTable";
 import { WorkingMansTable } from "@/4widgets/workingMansTable";
 import { CorrectionRaport } from "@/4widgets/correctionRaport";
 import { MonthPlan } from "@/4widgets/monthPlan";
 import { usePprTableSettings } from "@/2entities/ppr";
+
+import style from "./PprPage.module.scss";
 
 export const PprPage = () => {
   const { currentTimePeriod } = usePprTableSettings();
@@ -25,19 +28,37 @@ export const PprPage = () => {
 
   return (
     <Tabs
-      className="overflow-auto flex-1 print:overflow-visible"
+      className={clsx(style.fixAntdTab, "print:overflow-visible")}
       defaultActiveKey="2"
       destroyInactiveTabPane
       items={[
         {
           key: "1",
           label: "Настой часов",
-          children: <WorkingMansTable />,
+          children: (
+            <>
+              <h2 className="text-center font-bold text-xl mb-2">
+                ФОНД
+                <br />
+                рабочего времени
+              </h2>
+              <WorkingMansTable />
+            </>
+          ),
         },
         {
           key: "2",
           label: "План ТОиР",
-          children: <PprTable />,
+          children: (
+            <>
+              <h2 className="text-center font-bold text-xl mb-2">
+                ЗАТРАТЫ ТРУДА
+                <br />
+                на запланированные и фактически выполненные работы
+              </h2>
+              <PprTable />
+            </>
+          ),
         },
         {
           key: "3",
