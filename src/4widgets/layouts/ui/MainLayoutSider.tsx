869c7a37fbbb9ba1febac11db93b386c8d6e94ed
Spiryaev Artem <kaline-son@ya.ru>
2025-03-21 11:53:27 +0700

    fix: sider menu select item on startup

--- src/4widgets/layouts/ui/MainLayoutSider.tsx
+++ src/4widgets/layouts/ui/MainLayoutSider.tsx
@@ -1,10 +1,11 @@
 "use client";
-import { useState } from "react";
-import { Menu, MenuProps } from "antd";
-import { BarChartOutlined, HomeFilled, LogoutOutlined, TableOutlined } from "@ant-design/icons";
-import Sider from "antd/es/layout/Sider";
 import Link from "next/link";
+import { Menu, MenuProps } from "antd";
+import React, { useState } from "react";
+import Sider from "antd/es/layout/Sider";
 import { signOut } from "next-auth/react";
+import { usePathname } from "next/navigation";
+import { BarChartOutlined, HomeFilled, LogoutOutlined, TableOutlined } from "@ant-design/icons";
 
 import { ROUTE_PPR, ROUTE_REPORTS, ROUTE_ROOT } from "@/1shared/lib/routes";
 
@@ -20,7 +21,7 @@ function getItem(label: React.ReactNode, key: React.Key, icon?: React.ReactNode,
 
 const items: MenuItem[] = [
   getItem(<Link href={ROUTE_ROOT}>Главная</Link>, "home", <HomeFilled />),
-  getItem(<Link href={ROUTE_PPR}>Планы ТОиР</Link>, "year", <TableOutlined />),
+  getItem(<Link href={ROUTE_PPR}>Планы ТОиР</Link>, "ppr", <TableOutlined />),
   getItem(<Link href={ROUTE_REPORTS}>Отчеты</Link>, "reports", <BarChartOutlined />),
   getItem(<a onClick={() => signOut({ callbackUrl: ROUTE_ROOT })}>Выйти</a>, "logout", <LogoutOutlined />),
 ];
@@ -28,9 +29,22 @@ const items: MenuItem[] = [
 export const MainLayoutSider = ({ className }: { className: string }) => {
   const [isCollapsed, setIsCollapsed] = useState(true);
 
+  const pathname = usePathname();
+
+  const findDefaultSelectedKey = () => {
+    for (let i = 0; i < items.length; i++) {
+      if (pathname === "/") {
+        return ["home"];
+      }
+      if (pathname.startsWith(`/${items[i]?.key}`)) {
+        return [String(items[i]?.key)];
+      }
+    }
+  };
+
   return (
     <Sider className={className} collapsible collapsed={isCollapsed} onCollapse={(value) => setIsCollapsed(value)}>
-      <Menu theme="dark" mode="inline" items={items} />
+      <Menu defaultSelectedKeys={findDefaultSelectedKey()} theme="dark" mode="inline" items={items} />
     </Sider>
   );
 };
